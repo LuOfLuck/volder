48e7d6833e46786fbd906be1b42154ddd04c3152
Lucas <lcas83539@gmail.com>
2021-05-02 01:57:52 -0300

    Error de header en estudiante arreglado, y error (en todos los usuarios) del que header mobil no aparecia arreglado. Implemento de una lista de usuarios en el login

--- volder_app/static/js/ajax.js
+++ volder_app/static/js/ajax.js
@@ -1,18 +1,24 @@
 
-console.log("holamndo");
+console.log("holamndo")
 $(document).ready(function(){
     $("#sheare").submit(function(e){
         console.log("hola");
         e.preventDefault();
-        // $.ajax({
-        //     url: $(this).attr('action'),
-        //     type: $(this).attr('method'),
-        //     data: $(this).serialize(),
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
 
-        //     success: function(json){
-        //         console.log(json);
-        //     }
+            success: function(json, data){
+                let perfil = document.getElementById("perfil");
+                // perfil.innerHTML += ;
+                perfil.innerHTML = "";
+                json.forEach((element, i) => {
+                    console.log(element)
+                    perfil.innerHTML += element.nombre;
+                });
+            }
 
-        // })
+        })
     })
 })
